58d706d03fe602262e52ba4a3ab7754bbab1c58d
Bharath Sampath Kumar <39097638+BharathSampathKumar@users.noreply.github.com>
2020-07-08 12:21:42 -0700

    Added counter function

--- src/components/CycleCounter.js
+++ src/components/CycleCounter.js
@@ -2,12 +2,23 @@ import React from 'react';
 
 class CycleCounter extends React.Component {
 
+  constructor(props){
+    super(props);
+    this.state = {counter : 0};
+    this.onButtonClick = this.onButtonClick.bind(this);
+  }
+
+  onButtonClick(e){
+    let newVal = this.state.counter+1 == this.props.cycle ? 0 : this.state.counter+1;
+    this.setState({counter: newVal});
+  }
+
   render() {
     return (
       <button
         data-testid="cycle-counter"
         style={{ fontSize: '1rem', width: 120, height: 30, }}
-      >0</button>
+        onClick={this.onButtonClick}>{this.state.counter}</button>
     );
   }
 }
